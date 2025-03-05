314a7bb61a3c54363bb040397b4c65479d2e6603
Vrushali Parate <50298146+vrushaliparate@users.noreply.github.com>
2025-03-05 13:46:03 -0500

    SQL

---
+++ SQL/1633. Percentage of Users Attended a Contest.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select contest_id, round(count(user_id)*100.00/(select count(*) from users),2) as percentage
+from register
+group by contest_id
+order by percentage desc, contest_id;
