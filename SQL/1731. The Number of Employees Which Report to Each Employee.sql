d2a0a1d81e81be22a35526ab5c41b5f022ca957c
Vrushali Parate <50298146+vrushaliparate@users.noreply.github.com>
2025-03-07 10:47:47 -0500

    SQL

---
+++ SQL/1731. The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select e2.employee_id, e2.name,
+count(e1.employee_id) as reports_count, 
+round(avg(e1.age)) as average_age
+from employees e1 
+join employees e2
+on e1.reports_to = e2.employee_id
+group by e2.employee_id
+order by e2.employee_id;
